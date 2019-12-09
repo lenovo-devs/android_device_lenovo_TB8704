c71d9b706efc8dc56694077dd8ab4974bf77dfb9
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-12-09 19:59:24 +0100

    TB8704: build libunwind for gx_fpd
    Change-Id: If1d1bc2f9d8cb863b5215e6b981f00c88f4430a4

--- device.mk
+++ device.mk
@@ -35,7 +35,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.tb8704
+    android.hardware.biometrics.fingerprint@2.1-service.tb8704 \
+    libunwind
 
 # Fingerprint permissions
 PRODUCT_COPY_FILES += \
